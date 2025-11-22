c6f8b77768eb37112831af113bfc94c18a812e0b
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-22 20:04:39 +0530

    Even Or Odd

--- Introduction-to-Programming/EvenOrOdd.java
+++ Introduction-to-Programming/EvenOrOdd.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        int number = scanner.nextInt(); 
         System.out.println(number % 2 == 0 ? "Even" : "Odd");
         scanner.close();
     }
