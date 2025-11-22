48ded1f1cffa0605bbd7bbbfe1bebe3d5c69e77d
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-22 20:09:29 +0530

    Area Of Equilateral Triangle

---
+++ Conditionals-Loops/AreaOfEquilateralTriangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AreaOfEquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double side = sc.nextDouble();
+        double area = (Math.sqrt(3) / 4) * side * side;
+        System.out.println("Area: " + area);
+        sc.close();
+    }
+}
