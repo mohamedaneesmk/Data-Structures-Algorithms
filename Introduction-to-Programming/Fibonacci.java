dc755dc0f06d9c81d2007dc95ce7732d2216831d
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-22 20:04:55 +0530

    Fibonacci Series

--- Introduction-to-Programming/Fibonacci.java
+++ Introduction-to-Programming/Fibonacci.java
@@ -9,7 +9,7 @@ public class Fibonacci {
         int number = scanner.nextInt();
         for (int i = 0; i < number; i++) {
             System.out.print(fibonacci(i) + " ");
-        }
+        } 
 
         scanner.close();
     }
