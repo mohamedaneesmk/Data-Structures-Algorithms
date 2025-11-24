b03dc5fee79e9372022e10d645f55dded8e0b3e5
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-24 09:28:10 +0530

    Perimeter Of Square

--- Conditionals-Loops/PerimeterOfSquare.java
+++ Conditionals-Loops/PerimeterOfSquare.java
@@ -4,7 +4,7 @@ public class PerimeterOfSquare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double side = sc.nextDouble();
-        System.out.println("Perimeter: " + (4 * side));
+        System.out.println("Perimeter: " + (4 * side)); 
         sc.close();
     }
 }
