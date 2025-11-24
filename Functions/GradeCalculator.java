a279f948725944758f69775ac5cbc7918af1468f
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-24 11:21:46 +0530

    Grade Calculator

--- Functions/GradeCalculator.java
+++ Functions/GradeCalculator.java
@@ -25,7 +25,7 @@ public class GradeCalculator {
             return "A";
         } else if (marks >= 81) {
             return "B";
-        } else if (marks >= 71) {
+        } else if (marks >= 71) { 
             return "C";
         } else if (marks >= 61) {
             return "D";
