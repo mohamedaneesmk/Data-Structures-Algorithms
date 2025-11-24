910f20ac1ab6b125f70bdbfd1c26e90fc37a05d0
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-24 11:20:19 +0530

    Pythagorean Triplet Checker

--- Functions/PythagoreanTripletChecker.java
+++ Functions/PythagoreanTripletChecker.java
@@ -7,7 +7,7 @@ public class PythagoreanTripletChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter three numbers: ");
-        int a = sc.nextInt();
+        int a = sc.nextInt(); 
         int b = sc.nextInt();
         int c = sc.nextInt();
         
