d07c9fbe5a8abaa168b75d02fd836622f2d51178
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-24 11:27:41 +0530

    Even Or Odd Checker

--- Functions/EvenOddChecker.java
+++ Functions/EvenOddChecker.java
@@ -6,7 +6,7 @@ public class EvenOddChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int num = sc.nextInt();
+        int num = sc.nextInt(); 
         
         checkEvenOdd(num);
         sc.close();
