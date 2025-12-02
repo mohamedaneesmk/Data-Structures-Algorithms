dfdc848341e10d41bf9739dfff7a1afd6deeba37
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-02 11:32:39 +0530

    LeetCode #1773

---
+++ Arrays-ArrayList/Problems/CountItemsMatchingRule.java
@@ -0,0 +1,57 @@
+package Problems;
+
+import java.util.*;
+
+class CountItemsMatchingRule {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        // Map ruleKey to corresponding index
+        int index;
+        if (ruleKey.equals("type")) {
+            index = 0;
+        } else if (ruleKey.equals("color")) {
+            index = 1;
+        } else {
+            index = 2; // name
+        }
+
+        // Count items that match the rule
+        int count = 0;
+        for (List<String> item : items) {
+            if (item.get(index).equals(ruleValue)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // Test method
+    public static void main(String[] args) {
+        CountItemsMatchingRule solution = new CountItemsMatchingRule();
+
+        List<List<String>> items1 = Arrays.asList(
+                Arrays.asList("phone", "blue", "pixel"),
+                Arrays.asList("computer", "silver", "lenovo"),
+                Arrays.asList("phone", "gold", "iphone"));
+        System.out.println(solution.countMatches(items1, "color", "silver"));
+
+    }
+
+    // Alternative cleaner solution using HashMap
+    class SolutionOptimized {
+        public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+            // Create a mapping from ruleKey to index
+            Map<String, Integer> keyToIndex = new HashMap<>();
+            keyToIndex.put("type", 0);
+            keyToIndex.put("color", 1);
+            keyToIndex.put("name", 2);
+
+            int index = keyToIndex.get(ruleKey);
+
+            // Count matches using stream (Java 8+)
+            return (int) items.stream()
+                    .filter(item -> item.get(index).equals(ruleValue))
+                    .count();
+        }
+    }
+}
