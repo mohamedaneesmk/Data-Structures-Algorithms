7865f222fa8046f38b451f8e1420249533f3e669
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-02 16:28:00 +0530

    Transpose Matrix

--- Arrays-ArrayList/Problems/TransposeMatrix.java
+++ Arrays-ArrayList/Problems/TransposeMatrix.java
@@ -7,23 +7,26 @@ public class TransposeMatrix {
                 { 4, 5, 6 },
                 { 7, 8, 9 }
         };
-        transposeMatrix(matrix);
-    }
 
-    private static void transposeMatrix(int[][] matrix) {
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = row; col < matrix[0].length; col++) {
-                int temp = matrix[row][col];
-                matrix[row][col] = matrix[col][row];
-                matrix[col][row] = temp;
-            }
-        }
+        int[][] result = transposeMatrix(matrix);
 
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[0].length; col++) {
-                System.out.print(matrix[row][col] + " ");
+        for(int row = 0; row < result.length; row++){
+            for(int col = 0; col < result[0].length ; col++){
+                System.out.print(result[row][col] + " ");
             }
             System.out.println();
         }
+    }
+
+    private static int[][] transposeMatrix(int[][] matrix) {
+        int[][] result = new int[matrix[0].length][matrix.length];
+
+        for(int row = 0; row < matrix.length; row++){
+            for(int col = 0; col < matrix[0].length; col++){
+                result[col][row] = matrix[row][col];
+            }
+        }
+
+        return result;
     }
 }
