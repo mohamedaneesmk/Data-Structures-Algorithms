b21e1162618104724ce15fa01f3d4adca213bbb0
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-02 15:52:14 +0530

    Even Number Of Digits LeetCode #1295

--- Arrays-ArrayList/Problems/EvenNumberOfDigits.java
+++ Arrays-ArrayList/Problems/EvenNumberOfDigits.java
@@ -4,7 +4,7 @@ public class EvenNumberOfDigits {
     public static void main(String[] args) {
         int[] nums = { 555, 901, 482, 1771 };
         System.out.println(findEvenNumberOfDigits(nums));
-    }
+    } 
 
     private static int findEvenNumberOfDigits(int[] nums) {
         int evenDigitCount = 0;
