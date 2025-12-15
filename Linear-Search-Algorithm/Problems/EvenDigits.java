5cb4fcf4096cfbcef26a48eea09b44cfc53973d6
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-15 17:48:51 +0530

    Optimized Solution for LeetCode #1295

--- Linear-Search-Algorithm/Problems/EvenDigits.java
+++ Linear-Search-Algorithm/Problems/EvenDigits.java
@@ -1,31 +1,22 @@
 public class EvenDigits {
     public static void main(String[] args) {
-        int[] nums = { 12, 345, 2, 6, 7896 };
-        int result = findEvenDigits(nums);
-        System.out.println(result);
+        int[] nums = {12, 345, 2, 6, 7896};
+        System.out.println(findEvenDigits(nums));
     }
 
     private static int findEvenDigits(int[] nums) {
-        int evenDigitsCount = 0;
+        int count = 0;
 
         for (int num : nums) {
-            if (checkDigits(num) % 2 == 0) {
-                evenDigitsCount++;
+            if (digits(num) % 2 == 0) {
+                count++;
             }
         }
-
-        return evenDigitsCount;
+        return count;
     }
 
-    private static int checkDigits(int num) {
-        if (num == 0)
-            return 1;
-        int count = 0;
-        while (num != 0) {
-            num = num / 10;
-            count++;
-        }
-
-        return count;
+    private static int digits(int num) {
+        if (num == 0) return 1;
+        return (int) Math.log10(Math.abs(num)) + 1;
     }
 }
