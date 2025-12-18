b64205837de74156ee15cae6bd2efd84a3b29dec
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-18 22:27:35 +0530

    Guess Number Lower or Higher LeetCode #69

---
+++ Linear-Search-Algorithm/Problems/GuessTheNumber.java
@@ -0,0 +1,45 @@
+public class GuessTheNumber {
+
+    // Picked number (this simulates the hidden number)
+    private static int pickedNumber = 6;
+
+    public static void main(String[] args) {
+        int num = 10;
+        int result = findTheNumber(num);
+        System.out.println("Guessed Number: " + result);
+    }
+
+    private static int findTheNumber(int num) {
+        int low = 1;
+        int high = num;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int result = guess(mid);
+
+            if (result == 0)
+                return mid;
+            else if (result == 1)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    /**
+     * Mock implementation of guess API
+     * @param num guessed number
+     * @return -1 if num is higher than pickedNumber
+     *          1 if num is lower than pickedNumber
+     *          0 if correct
+     */
+    private static int guess(int num) {
+        if (num > pickedNumber)
+            return -1;
+        else if (num < pickedNumber)
+            return 1;
+        else
+            return 0;
+    }
+}
