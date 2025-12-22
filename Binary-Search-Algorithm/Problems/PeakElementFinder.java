1d4c8d52d4a442f95eedce04ace0434bd95697f8
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-22 20:53:38 +0530

    Find Peak Elemet | LeetCode #162

---
+++ Binary-Search-Algorithm/Problems/PeakElementFinder.java
@@ -0,0 +1,22 @@
+public class PeakElementFinder {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 1 };
+        System.out.println(findPeakElementFinder(nums));
+    }
+
+    private static int findPeakElementFinder(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] < nums[mid + 1])
+                start = mid + 1;
+            else
+                end = mid;
+        }
+
+        return start;
+    }
+}
