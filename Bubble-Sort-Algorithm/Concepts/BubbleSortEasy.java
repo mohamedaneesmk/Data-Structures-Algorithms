b69d067d4d1fbf8ed221db836b7510737a82666f
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-10 15:34:33 +0530

    Bubble Sort

--- Bubble-Sort-Algorithm/Concepts/BubbleSortEasy.java
+++ Bubble-Sort-Algorithm/Concepts/BubbleSortEasy.java
@@ -2,21 +2,24 @@
 
 public class BubbleSortEasy {
     public static void main(String[] args) {
-        int[] arr = {5, 3, 4, 1};
+        // int[] arr = { 5, 3, 4, 1 };
+        int[] arr = { 170, 150, 180, 160 };
+        bubbleSort(arr);
 
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    private static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
-        }
-
-        for (int num : arr) {
-            System.out.print(num + " ");
         }
     }
 }
