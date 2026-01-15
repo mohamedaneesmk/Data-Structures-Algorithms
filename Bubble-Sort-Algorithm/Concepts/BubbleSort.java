ec43e53f69d8e23a6e5d704b2787e27574cc8858
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-15 20:29:53 +0530

    Bubble Sort

--- Bubble-Sort-Algorithm/Concepts/BubbleSort.java
+++ Bubble-Sort-Algorithm/Concepts/BubbleSort.java
@@ -1,23 +1,25 @@
 // Buuble Sort Aslo known as Sinking Sort and Exchanging Sort 
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        int[] arr = { 170, 150, 180, 160 };
-        bubbleSort(arr);
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
 
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-    }
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
 
-    private static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    // Swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+
+            // If no swaps, array is sorted
+            if (!swapped) {
+                break;
             }
         }
     }
--- Selection-Sort-Algorithm/Concepts/SelectionSort.java
+++ Selection-Sort-Algorithm/Concepts/SelectionSort.java
@@ -8,29 +8,53 @@ public class SelectionSort {
         System.out.println(Arrays.toString(nums));
     }
 
-    private static void selectionSort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int last = nums.length - i - 1;
-            int maxIndex = getMaxIndex(nums, 0, last);
+    /*
+     * private static void selectionSort(int[] nums) {
+     * for (int i = 0; i < nums.length; i++) {
+     * int last = nums.length - i - 1;
+     * int maxIndex = getMaxIndex(nums, 0, last);
+     * 
+     * swap(nums, maxIndex, last);
+     * }
+     * }
+     * 
+     * private static void swap(int[] nums, int maxIndex, int last) {
+     * int temp = nums[maxIndex];
+     * nums[maxIndex] = nums[last];
+     * nums[last] = temp;
+     * }
+     * 
+     * private static int getMaxIndex(int[] nums, int start, int end) {
+     * int max = start;
+     * for (int i = start; i <= end; i++) {
+     * if (nums[max] < nums[i]) {
+     * max = i;
+     * }
+     * }
+     * 
+     * return max;
+     * }
+     */
 
-            swap(nums, maxIndex, last);
-        }
-    }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
-    private static void swap(int[] nums, int maxIndex, int last) {
-        int temp = nums[maxIndex];
-        nums[maxIndex] = nums[last];
-        nums[last] = temp;
-    }
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
 
-    private static int getMaxIndex(int[] nums, int start, int end) {
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if (nums[max] < nums[i]) {
-                max = i;
+            // Find minimum element in unsorted portion
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap only if minimum is not already in position
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
             }
         }
-
-        return max;
     }
 }
