f7b929e53affcdebf44cd67a36f6f709bb336bb7
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-18 08:15:09 +0530

    Cyclic Sort

---
+++ Cyclic-Sort-Algorithm/Concepts/CyclicSort.java
@@ -0,0 +1,40 @@
+public class CyclicSort {
+
+    public static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 2, 1, 4 };
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        cyclicSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+
+    private static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
