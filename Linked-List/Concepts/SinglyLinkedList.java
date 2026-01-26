6ac0bd73345ff8acfd0c421fa5529f474083e354
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-26 19:03:31 +0530

    Singly Linked List

---
+++ Linked-List/Concepts/SinglyLinkedList.java
@@ -0,0 +1,46 @@
+class SinglyLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.display();
+    }
+}
